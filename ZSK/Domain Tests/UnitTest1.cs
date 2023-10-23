cb6e0f38fdcc38665b31b4032f47ec0c05804a26
FilipMikosz <qfilipq2006@gmail.com>
2023-10-23 15:29:14 +0200

    lekja 23.10

---
+++ ZSK/Domain Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.NewDirectory;
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        int a = 10;
+        int b = 20;
+        int expectedResult = 10;
+
+        var result = GreatestCommonDivisor.GCDiterative(a, b);
+        Assert.Equal(expectedResult, result);
+    }
+} 
